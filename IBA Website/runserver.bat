44d1686faceabc2828341f9d29da4d987391face
SimoxG7 <simone.gioe@studenti.unimi.it>
2023-06-16 17:21:21 +0200

    added small util

---
+++ IBA Website/runserver.bat
@@ -0,0 +1,2 @@
+chcp 65001 &:: this is not to have any issues with non-ASCII characters
+node server.js
---
+++ IBA Website/runserverdesktop.bat
@@ -0,0 +1,4 @@
+chcp 65001 &:: this is not to have any issues with non-ASCII characters
+cd "IBA Test" 
+cd "IBA-Website"
+node server.js
